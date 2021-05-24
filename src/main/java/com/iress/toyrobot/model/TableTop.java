066871cfd29c88c0a78929b1217ed22bb7607672
Adam Cheung (Melb) <adam.c.cheung@gmail.com>
2021-05-24 11:29:38 +1000

    Add TableTop model

---
+++ src/main/java/com/iress/toyrobot/model/TableTop.java
@@ -0,0 +1,10 @@
+package com.iress.toyrobot.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TableTop {
+    private byte[] dimensions;
+}
---
+++ src/main/java/com/iress/toyrobot/service/AccidentAvoidanceService.java
@@ -0,0 +1,10 @@
+package com.iress.toyrobot.service;
+
+import com.iress.toyrobot.model.TableTop;
+import com.iress.toyrobot.model.ToyRobot;
+
+public class AccidentAvoidanceService {
+    public boolean preventFromFalling(TableTop tableTop, ToyRobot toyRobot) {
+        return true;
+    }
+}
